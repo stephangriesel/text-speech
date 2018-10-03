0bc8e3312b73fc466db84b462e5f52686c3144f6
Stephan Griesel <sgriesel@gmail.com>
2018-10-03 07:30:38 +0200

    set dom elements

---
+++ script.js
@@ -0,0 +1,12 @@
+// Var for SpeechAPI, more info on API can be found here: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+const speech = window.speechSynthesis;
+
+// DOM elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const textForm = document.querySelector('#rate-value');
+const textForm = document.querySelector('#rate');
+const textForm = document.querySelector('#pitch');
+const textForm = document.querySelector('#pitch-value');
+const textForm = document.querySelector('#voice-select');
+
